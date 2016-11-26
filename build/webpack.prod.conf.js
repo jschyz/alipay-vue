7b3d6f5b04328cd2b2841d0d6b9659f4769d0898
huihui <skyalpha@126.com>
2016-11-26 21:25:12 +0800

    fine-tuning

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const config = require('./config')
 const baseWebpackConfig = require('./webpack.base.conf')
 
 let htmlPlugin = []
